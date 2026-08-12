27e5212c86123eeb85841019ed9a901233cad129
Mary <mary.raymond.n@gmail.com>
2026-08-12 22:21:54 +0000

    Fixing the name of the experiments

--- scripts/benchmarks/run_benchmarks.sh
+++ scripts/benchmarks/run_benchmarks.sh
@@ -5,43 +5,43 @@
 
 ROOT_DIR="$1"
 SCENE_NAME="$2"
-EXP_NAME="$3"
-
-sh /code/scripts/run_bm_poses_trace.sh ${ROOT_DIR} ${SCENE_NAME} ${EXP_NAME} 30000
-
-
-python3 /code/scripts/src/benchmark_table_poses.py \
-	--root /workspace/outputs/${EXP_NAME} \
-	--out /workspace/outputs/${EXP_NAME}/results_bm_poses.csv
-	
-	
-python3 /code/scripts/src/benchmark_qualitative_poses.py \
-    --root /workspace/outputs/${EXP_NAME} \
-    --out-dir /workspace/outputs/${EXP_NAME}/output_plots 
-
-EXP_NAME="exp_ds_bm_cam_cal"
-sh /code/scripts/run_bm_cam_calibr_trace.sh ${ROOT_DIR} ${SCENE_NAME} ${EXP_NAME} 30000
-
-
-python3 /code/scripts/src/benchmark_table_cam_calibr.py \
-	--root /workspace/outputs/${EXP_NAME} \
-	--out /workspace/outputs/${EXP_NAME}/results_bm_poses.csv
-	
-	
-python3 /code/scripts/src/benchmark_qualitative_cam_calibr.py \
-    --root /workspace/outputs/${EXP_NAME} \
-    --out-dir /workspace/outputs/${EXP_NAME}/output_plots 
-
 
 EXP_NAME="exp_ds_bm_nv"
-sh /code/scripts/run_bm_nv_trace.sh ${ROOT_DIR} ${SCENE_NAME} ${EXP_NAME} 60000
+sh run_bm_nv_trace.sh ${ROOT_DIR} ${SCENE_NAME} ${EXP_NAME} 60000
 
 
-python3 /code/scripts/src/benchmark_table_nv.py \
+python3 summarize_results/benchmark_table_nv.py \
 	--root /workspace/outputs/${EXP_NAME} \
 	--out /workspace/outputs/${EXP_NAME}/results_bm_nv.csv
 	
 	
-python3 /code/scripts/src/benchmark_qualitative_nv.py \
+python3 summarize_results/benchmark_qualitative_nv.py \
     --root /workspace/outputs/${EXP_NAME} \
     --out-dir /workspace/outputs/${EXP_NAME}/output_plots 
+
+sh run_bm_poses_trace.sh ${ROOT_DIR} ${SCENE_NAME} ${EXP_NAME} 30000
+
+
+python3 summarize_results/benchmark_table_poses.py \
+	--root /workspace/outputs/${EXP_NAME} \
+	--out /workspace/outputs/${EXP_NAME}/results_bm_poses.csv
+	
+	
+python3 summarize_results/benchmark_qualitative_poses.py \
+    --root /workspace/outputs/${EXP_NAME} \
+    --out-dir /workspace/outputs/${EXP_NAME}/output_plots 
+
+EXP_NAME="exp_ds_bm_cam_cal"
+sh run_bm_cam_calibr_trace.sh ${ROOT_DIR} ${SCENE_NAME} ${EXP_NAME} 30000
+
+
+python3 summarize_results/src/benchmark_table_cam_calibr.py \
+	--root /workspace/outputs/${EXP_NAME} \
+	--out /workspace/outputs/${EXP_NAME}/results_bm_poses.csv
+	
+	
+python3 summarize_results/benchmark_qualitative_cam_calibr.py \
+    --root /workspace/outputs/${EXP_NAME} \
+    --out-dir /workspace/outputs/${EXP_NAME}/output_plots 
+
+
