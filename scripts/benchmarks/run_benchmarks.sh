8600d65c08de8cd45d82eb5ac24001c4c4a2bc0f
Mary <mary.raymond.n@gmail.com>
2026-08-16 17:41:33 +0000

    Removing the benchmark summaries since they are outdates

--- scripts/benchmarks/run_benchmarks.sh
+++ scripts/benchmarks/run_benchmarks.sh
@@ -10,39 +10,40 @@ EXP_NAME="exp_ds_bm_nv"
 run_bm_nv_trace ${ROOT_DIR} ${SCENE_NAME} ${EXP_NAME} 60000
 
 
-python3 summarize_results/benchmark_table_nv.py \
-	--root /workspace/outputs/${EXP_NAME} \
-	--out /workspace/outputs/${EXP_NAME}/results_bm_nv.csv
+# python3 summarize_results/benchmark_table_nv.py \
+# 	--root /workspace/outputs/${EXP_NAME} \
+# 	--out /workspace/outputs/${EXP_NAME}/results_bm_nv.csv
 	
 	
-python3 summarize_results/benchmark_qualitative_nv.py \
-    --root /workspace/outputs/${EXP_NAME} \
-    --out-dir /workspace/outputs/${EXP_NAME}/output_plots 
+# python3 summarize_results/benchmark_qualitative_nv.py \
+#     --root /workspace/outputs/${EXP_NAME} \
+#     --out-dir /workspace/outputs/${EXP_NAME}/output_plots 
+
 EXP_NAME="exp_ds_bm_poses"
 
 run_bm_poses_trace ${ROOT_DIR} ${SCENE_NAME} ${EXP_NAME} 30000
 
 
-python3 summarize_results/benchmark_table_poses.py \
-	--root /workspace/outputs/${EXP_NAME} \
-	--out /workspace/outputs/${EXP_NAME}/results_bm_poses.csv
+# python3 summarize_results/benchmark_table_poses.py \
+# 	--root /workspace/outputs/${EXP_NAME} \
+# 	--out /workspace/outputs/${EXP_NAME}/results_bm_poses.csv
 	
 	
-python3 summarize_results/benchmark_qualitative_poses.py \
-    --root /workspace/outputs/${EXP_NAME} \
-    --out-dir /workspace/outputs/${EXP_NAME}/output_plots 
+# python3 summarize_results/benchmark_qualitative_poses.py \
+#     --root /workspace/outputs/${EXP_NAME} \
+#     --out-dir /workspace/outputs/${EXP_NAME}/output_plots 
 
 EXP_NAME="exp_ds_bm_cam_cal"
 run_bm_cam_calibr_trace ${ROOT_DIR} ${SCENE_NAME} ${EXP_NAME} 30000
 
 
-python3 summarize_results/src/benchmark_table_cam_calibr.py \
-	--root /workspace/outputs/${EXP_NAME} \
-	--out /workspace/outputs/${EXP_NAME}/results_bm_poses.csv
+# python3 summarize_results/src/benchmark_table_cam_calibr.py \
+# 	--root /workspace/outputs/${EXP_NAME} \
+# 	--out /workspace/outputs/${EXP_NAME}/results_bm_poses.csv
 	
 	
-python3 summarize_results/benchmark_qualitative_cam_calibr.py \
-    --root /workspace/outputs/${EXP_NAME} \
-    --out-dir /workspace/outputs/${EXP_NAME}/output_plots 
+# python3 summarize_results/benchmark_qualitative_cam_calibr.py \
+#     --root /workspace/outputs/${EXP_NAME} \
+#     --out-dir /workspace/outputs/${EXP_NAME}/output_plots 
 
 
